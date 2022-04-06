c4541fcf66f7712db70632c477ef9eff9c707724
master-of-null <gabriel@digital-dash.co>
2022-04-06 17:01:37 -0400

    adds controller service to catch asynchronous controller actions

--- src/controllers/acronyms.js
+++ src/controllers/acronyms.js
@@ -1,32 +1,33 @@
 const acronymService = require('../services/acronym')
+const catchAsync = require('./services/catch-async')
 
-const createAcronym = async (req, res) => {
+const createAcronym = catchAsync(async (req, res) => {
   const { value, description } = req.body
   await acronymService.createAcronym(value, description)
   res.send('Success!')
-}
+})
 
-const getAcronyms = async (req, res) => {
+const getAcronyms = catchAsync(async (req, res) => {
   const { limit, from: offset } = req.query
 
   const { count } = await acronymService.getCount()
   const results = await acronymService.getAcronyms(offset, limit)
 
   res.send({ results, count })
-}
+})
 
-const updateAcronym = async (req, res) => {
+const updateAcronym = catchAsync(async (req, res) => {
   const { acronym } = req.params
   const { description } = req.body
   await acronymService.updateAcronym(acronym, description)
   res.send('Success!')
-}
+})
 
-const deleteAcronym = async (req, res) => {
+const deleteAcronym = catchAsync(async (req, res) => {
   const { acronym } = req.params
   await acronymService.deleteAcronym(acronym)
   res.send('Success!')
-}
+})
 
 module.exports = {
   createAcronym,
